50fa6e1e46a0a0dea131a7a2a3641dc2227e1e50
devilutionz <74042079+devilutionz@users.noreply.github.com>
2024-04-02 23:49:58 +0700

    Update Head.tsx

--- components/Head.tsx
+++ components/Head.tsx
@@ -16,8 +16,8 @@ type Props = {
  */
 export const Head: FC<Props> = ({
   ogImage = 'https://explorer.reservoir.tools/og-image.png',
-  title = 'Reservoir | Multi-Chain NFT Explorer',
-  description = 'Reservoir Multi-Chain NFT Explorer is an open source NFT explorer built with Reservoir.',
+  title = 'Historics.xyz - Important Crypto Collectibles',
+    description = 'Discover and explore historic crypto collectibles with the community.',
   metatags = null,
 }) => {
   return (
